3ba3a559833447a362ab9525f9d215d143b453c2
andres <acmejia.xpe@gmail.com>
2021-05-27 18:46:18 -0500

    heroku 2

--- banco_usuario/settings.py
+++ banco_usuario/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6q9v-sup@w49+s*t)8^nxefip9q-%^)r%kp1n)04%g4xmd*_!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
