e682907421254e4716deed1a05e2360a1c3761d6
andres <acmejia.xpe@gmail.com>
2021-07-15 10:59:56 -0500

    corsheaders3

--- banco_usuario/settings.py
+++ banco_usuario/settings.py
@@ -196,8 +196,16 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = default_headers + (
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
     'token',
-    'authorization'
-)
+]
 
